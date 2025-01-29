7806564a3e5345417fb18f654ec9145e602e5e59
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-01-29 11:00:24 +0100

    add useEffect to fetch & create temp map to show data

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,35 @@
+import {useEffect, useState} from "react";
+import {fetchLogements} from "../../data/fetchLogements";
+import {Link} from "react-router-dom";
+import {routes} from "../../router/routes";
+
 const Home = () => {
-    return <h1>Home Page</h1>
+    const [logements, setLogements] = useState<any[]>([])
+
+    useEffect(() => {
+        const loadLogements = async () => {
+            const data = await fetchLogements()
+            setLogements(data)
+        }
+        loadLogements()
+    }, [])
+
+    return (
+        <>
+            <div className={"container mt-5"}>
+                {/* TODO: hero + titre -> container + children */}
+                {/* TODO: map card component logements */}
+                <div className={"row g-3"}>
+                    {logements.map((logement, i) => (
+                        <div className={"col-md-4"} key={i}>
+                            <h2>{logement.title}</h2>
+                            <Link to={routes.logement(logement.id)}>Voir l'annonce</Link>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Home
