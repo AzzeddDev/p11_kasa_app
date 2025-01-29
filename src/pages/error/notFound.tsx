aaf4578c11deeedc51a097befbd98c6ef87a417d
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-01-29 10:59:06 +0100

    front changes 404 error page

--- src/pages/error/notFound.tsx
+++ src/pages/error/notFound.tsx
@@ -4,9 +4,10 @@ import {routes} from "../../router/routes"
 const NotFound = () => {
     return (
         <div className={"text-center mt-5"}>
-            <h1 className={"text-danger"}>404 - Page non trouver</h1>
-            <p>La page que vous essayer de chercher n'a pas été trouver</p>
-            <Link to={routes.home} className={"btn btn-primary"}>Retour a l'accueil</Link>
+            {/* TODO: re-designer la page 404 */}
+            <h1 className={"text-danger"}>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to={routes.home} className={"btn"}>Retourner sur la page d’accueil</Link>
         </div>
     )
 }
