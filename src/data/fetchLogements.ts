f0f67134cf1794b2e535e3b58f0fb78eb385ae61
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-01-29 10:57:24 +0100

    add api fetch data

---
+++ src/data/fetchLogements.ts
@@ -0,0 +1,12 @@
+export const fetchLogements = async () => {
+    try {
+        const response = await fetch("/data/logements.json")
+        if (!response.ok) {
+            throw new Error("Erreur lors du chargement des logements")
+        }
+        return await response.json()
+    } catch (err) {
+        console.error("Erreur de chargement :", err)
+        return []
+    }
+}
