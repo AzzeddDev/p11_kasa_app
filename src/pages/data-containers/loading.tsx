cd007b8df7b01962d4b90b7a26b9a1da65e12adc
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-01-30 15:59:13 +0100

    add a loading/spinner data container

---
+++ src/pages/data-containers/loading.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Loading = () => (
+    <div className={"container"}>
+        <div className="loadingContainer">
+            <div className="loadingSpinner"/>
+            <div>Loading...</div>
+        </div>
+    </div>
+)
+
+export default Loading
