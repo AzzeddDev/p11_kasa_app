737c72fcc1328aee7f183e587a6d55dec2d3cf54
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-01-30 16:00:49 +0100

    import personalized hooks & improve code

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,30 +1,28 @@
-import {useEffect, useState} from "react"
-import {fetchLogements} from "../../data/fetchLogements"
-import {Link} from "react-router-dom"
+import useFetchLogements from "../../data/hooks/useFetchLogements"
+import Card from "../../components/card"
 import {routes} from "../../router/routes"
+import Loading from "../data-containers/loading"
 
 const Home = () => {
-    const [logements, setLogements] = useState<any[]>([])
-
-    useEffect(() => {
-        const loadLogements = async () => {
-            const data = await fetchLogements()
-            setLogements(data)
-        }
-        loadLogements()
-    }, [])
+    const { logements, loading, error } = useFetchLogements()
 
     return (
         <>
             <div className={"container mt-5"}>
                 {/* TODO: hero + titre -> container + children */}
                 {/* TODO: map card component logements */}
-                <div className={"row g-3"}>
+
+                {loading && <Loading />}
+                {error && <p className={"text-danger"}>{error}</p>}
+
+                <div className={"row g-5"}>
                     {logements.map((logement, i) => (
-                        <div className={"col-md-4"} key={i}>
-                            <h2>{logement.title}</h2>
-                            <Link to={routes.logement(logement.id)}>Voir l'annonce</Link>
-                        </div>
+                        <Card
+                            key={i}
+                            title={logement.title}
+                            to={routes.logement(logement.id)}
+                            backgroundUrl={logement.cover}
+                        />
                     ))}
                 </div>
             </div>
--- src/pages/logement/index.tsx
+++ src/pages/logement/index.tsx
@@ -1,34 +1,14 @@
 import {useParams} from "react-router-dom"
-import {useEffect, useState} from "react"
-import {fetchLogements} from "../../data/fetchLogements"
-import NotFound from "../error/notFound";
-
-interface LogementType {
-    id: string
-    title: string
-    cover: string
-    location: string
-    description: string
-    pictures: string[]
-}
+import NotFound from "../data-containers/notFound"
+import useFetchSingleLogement from "../../data/hooks/useFetchSingleLogement"
 
 export default function Logement () {
     const { id } = useParams<{ id: string }>()
-    const [ logement, setLogement ] = useState<LogementType | null>(null)
+    const { logement, loading, error } = useFetchSingleLogement(id)
 
-    useEffect(() => {
-        const loadLogement = async () => {
-            const logements = await fetchLogements()
-            const foundLogement = logements.find((l: LogementType) => l.id === id)
-            setLogement( foundLogement || null )
-        }
-
-        loadLogement()
-    }, [id])
-
-    if (!logement) {
-        return (<NotFound/>)
-    }
+    if (loading) return <p>Chargement...</p>
+    if (error) return <p className={"text-danger"}>{error}</p>
+    if (!logement) return <NotFound/>
 
     return (
         <>
