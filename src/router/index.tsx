1a68dfc247181fd86e44b80897c423f2779621f5
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-01-30 09:34:38 +0100

    add about Route in router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,11 +3,13 @@ import Home from "../pages/home"
 import {routes} from "./routes"
 import NotFound from "../pages/error/notFound"
 import Logement from "../pages/logement"
+import About from "../pages/about"
 
 export default function Router() {
     return (
         <Routes>
             <Route path={routes.home} element={<Home />}/>
+            <Route path={routes.about} element={<About />}/>
             <Route path={routes.logement()} element={<Logement />}/>
             <Route path={routes.notFound} element={<NotFound />}/>
         </Routes>
