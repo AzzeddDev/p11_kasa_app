791792820dd7fedfdc3211befba1700f1fa0fb67
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-02-01 18:15:18 +0100

    update file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/p11_kasa_app/',
   server: {
     watch: {
       usePolling: true,
