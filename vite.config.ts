512fdeacab3db96447bc695a5062f414a748a3cd
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-02-01 18:13:55 +0100

    try fix github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/p11_kasa_app/',
   server: {
     watch: {
       usePolling: true,
