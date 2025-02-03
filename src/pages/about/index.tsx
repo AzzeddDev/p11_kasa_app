fb68bb3cc3d9c26a14eebb9e0100b062398a49a1
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-02-03 10:42:54 +0100

    add accodions in about-us template

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,5 +1,6 @@
 import AboutImgHero from "../../assets/img/image-source-header-a-propos.png"
 import Header from "../../containers/header"
+import Accordion from "../../components/accordion";
 
 export default function About() {
     return (
@@ -11,6 +12,30 @@ export default function About() {
                     bgImage={AboutImgHero}
                     title={"A propos"}
                 />
+
+                <div className={"col d-flex flex-column align-items-center mb-5"}>
+                    <div className={"col-md-10 d-flex flex-column gap-4"}>
+                        <Accordion
+                            title={"Fiabilité"}
+                            content={"Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."}
+                        />
+
+                        <Accordion
+                            title={"Respect"}
+                            content={"La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."}
+                        />
+
+                        <Accordion
+                            title={"Service"}
+                            content={"Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."}
+                        />
+
+                        <Accordion
+                            title={"Sécurité"}
+                            content={"La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."}
+                        />
+                    </div>
+                </div>
             </div>
         </>
     )
