859d66f69cc0e1ca85f536e263fd841b2c607571
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-02-04 12:57:55 +0100

    update Button composent className variable

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,8 +1,8 @@
 import {Link} from "react-router-dom"
 
-const Button = ({to, children}) => {
+const Button = ({to, className, children}) => {
     return (
-        <Link to={to} className={"buttonDefault"}>
+        <Link to={to} className={className}>
             {children}
         </Link>
     )
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -14,8 +14,12 @@ const Navbar = () => {
                     <img className={"navBar__logo"} src={logo} alt=""/>
                 </Link>
                 <div className="d-flex gap-5">
-                    <Button to={routes.home}>Accueil</Button>
-                    <Button to={routes.about}>A propos</Button>
+                    <Button to={routes.home} className={"buttonNavbar"}>
+                        Accueil
+                    </Button>
+                    <Button to={routes.about} className={"buttonNavbar"}>
+                        A propos
+                    </Button>
                 </div>
             </nav>
         </>
