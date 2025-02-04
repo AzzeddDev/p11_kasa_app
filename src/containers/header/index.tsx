fcf6b33212eb11433e06e07918ff27002a817fb7
Azz Eddine SALMI <salmi.azzeddine100@gmail.com>
2025-02-04 13:11:09 +0100

    update $ variable props

--- src/containers/header/index.tsx
+++ src/containers/header/index.tsx
@@ -2,13 +2,13 @@ import React from "react"
 import styled from "styled-components"
 
 const Hero = styled.div`
-  background-image: url(${(props) => props.bgImage});
+  background-image: url(${(props) => props.$bgImage});
 `
 
 const Header = ({ bgImage, title }) => {
     return (
         <section className={"col-12"}>
-            <Hero bgImage={bgImage} className={"headerHero"}>
+            <Hero $bgImage={bgImage} className={"headerHero"}>
                 <h1 className="bigTitle">{title}</h1>
             </Hero>
         </section>
